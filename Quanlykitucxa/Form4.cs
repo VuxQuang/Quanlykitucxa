d0598b16b269c110c6b04395be89467f8c7028c2
VuxQuang <124116021+VuxQuang@users.noreply.github.com>
2023-11-26 14:51:43 +0700

    Add files via upload

---
+++ Quanlykitucxa/Form4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quanlykitucxa
+{
+
+    public partial class Form4 : Form
+    {
+        String query;
+        function fn = new function();
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExist_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(470, 110);
+            query = "SELECT * FROM newStudent WHERE living = 'Yes'";
+            DataSet ds = fn.getData(query);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
